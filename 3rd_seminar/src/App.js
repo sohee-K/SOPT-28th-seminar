63300df47e74da4a33a2c8c5fb06fad255d8b7d1
Sohee KWON <soheekwon@naver.com>
2021-04-24 11:09:28 +0900

    [step 3] API 결과 불러오기 + axios 설치

--- 3rd_seminar/src/App.js
+++ 3rd_seminar/src/App.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchBar from "./components/SearchBar";
 import ResultCard from "./components/ResultCard";
+import { getUserData } from "./lib/api/githubAPI";
 
 function App() {
+  useEffect(() => {
+    getUserData("sohee-K");
+  }, []);
+
   return (
     <>
       <SearchBar />
---
+++ 3rd_seminar/src/lib/api/githubAPI.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: "https://api.github.com/users/",
+});
+
+export const getUserData = async (name) => {
+  try {
+    const { data } = await client.get(name);
+    console.log("[SUCCESS] GET user data", data);
+    return data.data;
+  } catch (e) {
+    console.log("[FAIL] GET user data", e);
+  }
+};
