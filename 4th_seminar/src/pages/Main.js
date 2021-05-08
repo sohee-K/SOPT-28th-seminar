9988301f7588f32de32f57b83e6b4ac368b0b6d9
Sohee KWON <soheekwon@naver.com>
2021-05-08 13:51:08 +0900

    [step 6] Card 구현하기

--- 4th_seminar/src/pages/Main.js
+++ 4th_seminar/src/pages/Main.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Card from "../components/main/Card";
+import Styled from "styled-components";
+
+const MainWrap = Styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, auto);
+  row-gap: 25px;
+`;
 
 const Main = () => {
-  return <div>Main Page</div>;
+  const [userData, setUserData] = React.useState({
+    id: 1,
+    date: 20210501,
+    title: "다이어리 제목 어쩌고 저쩌고",
+    image: "",
+    weather: "맑음",
+    tags: ["태그1", "태그2"],
+  });
+
+  return (
+    <MainWrap>
+      <Card props={userData} />;
+    </MainWrap>
+  );
 };
 
 export default Main;
