58059efef5982a9c6bd78fd6d46e84e4ce944f29
Sohee KWON <soheekwon@naver.com>
2021-05-08 13:41:58 +0900

    [step 5] Calendar 구현하기

--- 4th_seminar/src/App.js
+++ 4th_seminar/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MainHeader from "./components/common/MainHeader";
 import Calendar from "./components/common/Calendar";
@@ -7,12 +7,26 @@ import Footer from "./components/common/Footer";
 import Main from "./pages/Main";
 import Diary from "./pages/Diary";
 
+const getCurrDate = () => {
+  const now = new Date();
+  const currYear = now.getFullYear();
+  const currMonth = now.getMonth();
+  return { year: currYear, month: currMonth };
+};
+
 function App() {
+  const [year, setYear] = useState(getCurrDate().year);
+  const [month, setMonth] = useState(getCurrDate().month);
   return (
     <>
       <BrowserRouter>
         <MainHeader />
-        <Calendar />
+        <Calendar
+          currYear={year}
+          setCurrYear={setYear}
+          currMonth={month}
+          setCurrMonth={setMonth}
+        />
         <Title />
         <Switch>
           <Route exact path="/" component={Main} />
