a0f2016a51a9add22752f46151ebdfd8d77084f7
Sohee KWON <soheekwon@naver.com>
2021-05-08 13:14:37 +0900

    [step 2] 전체 Component 구조 정하기

--- 4th_seminar/src/App.js
+++ 4th_seminar/src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import MainHeader from "./components/common/MainHeader";
+import Calendar from "./components/common/Calendar";
+import Title from "./components/common/Title";
+import Footer from "./components/common/Footer";
+import Main from "./pages/Main";
+import Diary from "./pages/Diary";
 
 function App() {
-  return <div className="App">Diary App</div>;
+  return (
+    <>
+      <MainHeader />
+      <Calendar />
+      <Title />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/diary" component={Diary} />
+          <Route path="/diary/:id" component={Diary} />
+        </Switch>
+      </BrowserRouter>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
